501a03841bfe4efd06a9ca0b6d56fe5932347ad9
DiggieIsSavage <122311339+DiggieIsSavage@users.noreply.github.com>
2023-01-10 05:04:22 +0800

    Update streamlit.app.py
    Added comments as well as the picklist

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,12 +1,21 @@
+#importing libraries
 import streamlit
 import pandas as pd
 
+#declaring variables
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-
+#displaying objects in streamlit
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menu")
 streamlit.text( "🥞 Omega 3 & Blueberry Oatmeal" + "\n" + "🥗" + "Kale, Spinach & Rocket Smoothie" + "\n" + "🐔" + "Hard Boiled Free-Range Eggs"
               + "\n" + "🥑" + "🍞" + "Avocado Toast")  
 streamlit.header("🍌 🍎 Build your own smoothie 🥝 🍇") 
+
+#let's put a picklist here so they can pick the fruit they want to include
+streamlit.multiselect("Pick some fruits", list(my_fruit_list.index))
+
+#display the table on the page
 streamlit.dataframe(my_fruit_list)
+
+

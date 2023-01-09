c3374b4f02250034dab6f2ee8de6f45febe1d89a
DiggieIsSavage <122311339+DiggieIsSavage@users.noreply.github.com>
2023-01-10 04:59:11 +0800

    Update streamlit.app.py
    Added fruit list

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,6 +1,12 @@
 import streamlit
+import pandas as pd
+
+myfruitlist = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menu")
 streamlit.text( "🥞 Omega 3 & Blueberry Oatmeal" + "\n" + "🥗" + "Kale, Spinach & Rocket Smoothie" + "\n" + "🐔" + "Hard Boiled Free-Range Eggs"
               + "\n" + "🥑" + "🍞" + "Avocado Toast")  
 streamlit.header("🍌 🍎 Build your own smoothie 🥝 🍇") 
+streamlit.dataframe(my_fruit_list)

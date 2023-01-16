b4395bc1b231a967c46c4da3c90eaab726524785
DiggieIsSavage <122311339+DiggieIsSavage@users.noreply.github.com>
2023-01-16 18:34:38 +0800

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -24,12 +24,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #putting in new header and text in new app
-streamlit.text(fruityvice_response)
+# streamlit.text(fruityvice_response) # will not show a value as you need to work on the format
 streamlit.header('Fruityvice Fruit Advice!')
 streamlit.text(fruityvice_response.json())
 
 #take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 #output it in the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 

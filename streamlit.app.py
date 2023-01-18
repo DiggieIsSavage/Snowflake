1f86eaeda4009b25e2a07e126fb7f67fa3461e45
DiggieIsSavage <122311339+DiggieIsSavage@users.noreply.github.com>
2023-01-18 16:54:36 +0800

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -28,17 +28,22 @@ streamlit.dataframe(fruits_to_show)
 #putting in new header and text in new app
 # streamlit.text(fruityvice_response) # will not show a value as you need to work on the format
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-streamlit.write('The user entered ', fruit_choice)
 
-#streamlit.text(fruityvice_response.json())
+try: 
+  fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else: 
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  #streamlit.write('The user entered ', fruit_choice)
+  #streamlit.text(fruityvice_response.json())
+  #take the json version of the response and normalize it
+  fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+  #output it in the screen as a table
+  streamlit.dataframe(fruityvice_normalized)
 
-#take the json version of the response and normalize it
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-#output it in the screen as a table
-streamlit.dataframe(fruityvice_normalized)
-
+except URLError as e: 
+  streamlit.error()
 
 #to stop a streamlit code below during troubleshooting use the script below
 streamlit.stop()
